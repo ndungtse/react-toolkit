0bbc362e3292760450849f36cc67ce4ce224c46a
ndungtse <ndungutsecharles103@gmail.com>
2024-06-16 18:00:47 +0200

    add data-table

---
+++ src/utils/funcs/index.ts
@@ -0,0 +1,58 @@
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+
+/**
+ * function that takes a key string like 'name' or 'name.first' and returns a function that takes an object and returns the value of the key in the object
+ * @param key - the key to get the value of you cane dots for nesting values
+ * @param obj - the object to get the value from
+ */
+export const getObjValue = <T = any>(key: string | number, obj: any) => {
+   const keys = key.toString().split('.');
+   let result = obj;
+   for (const key of keys) {
+      if (result && Object.prototype.hasOwnProperty.call(result, key)) {
+         result = result[key];
+      } else {
+         return undefined;
+      }
+   }
+   return result as T;
+};
+
+/**
+ * @param error
+ * @returns  a string error from response
+ */
+export const getResError = (error?: any) => {
+   if (!error) return 'Something Went Wrong';
+   const isNetError = error?.message?.includes('Network Error');
+   if (isNetError) return 'Network Error';
+   return error?.response?.data?.error ?? error?.response?.data?.message ?? error?.message ?? 'Something Went Wrong';
+};
+
+export const decodeToken = (_token?: string) => {
+   let data: any = null;
+   try {
+      const token = _token ?? Cookies.get('token');
+      if (!token) {
+         return data;
+      }
+      const decoded = jwtDecode(token.toString());
+      data = decoded;
+   } catch (error) {
+      console.error('Error decoding token', error);
+   }
+   return data;
+};
+
+/**
+ * @param str
+ * @returns a capitalize string ex: 'HELLO_WORLD' => 'Hello World'
+ */
+export const capitalize = (str: string): string => {
+   if (!str) return '';
+   return str
+      ?.split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+};
